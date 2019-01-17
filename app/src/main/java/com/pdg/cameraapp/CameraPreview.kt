513904e01ae1af1352a73a993492014b47d7a359
Pramod Guruprasad <pdg.august@gmail.com>
2019-01-17 14:51:26 +0100

    Initial commit

---
+++ app/src/main/java/com/pdg/cameraapp/CameraPreview.kt
@@ -0,0 +1,11 @@
+package com.pdg.cameraapp
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class CameraPreview : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
